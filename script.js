6a2a108e15fb6d337da495015c545fe8815de854
vlad <vlad.markuts@gmail.com>
2020-04-18 20:49:25 +0300

    refactoring + tooltip

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-		for (var i = 0; i < circlesNumber; i++) {
-			circles[i] = drawCircle(x,y);
-			for (var j = 0; j < circlesNumber; j++) {
-				circles[j] = drawCircle(x,y);
-				y+=change;
-			}
-			y = 0;
-			x+=change;
-		}
+var tooltip = document.getElementById('tooltip');
 
-		function drawCircle(xCord, yCord) {
-			cirlce = new Path.Circle(new Point(xCord, yCord), r);
-			cirlce.fillColor = 'purple';
-			return cirlce;
-		}
+document.addEventListener('click', function() {
+	document.getElementsByTagName('body')[0].focus();
+	document.getElementById('tooltip').setAttribute('style','display: none');
+	document.getElementById('canvasFrame').focus();
+});
